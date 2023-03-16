c9053af16c749cedae4531be7efd20dcb0498b93
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-03-16 19:00:02 +0700

    chane request for user

--- api/app/Http/Requests/UserRequest.php
+++ api/app/Http/Requests/UserRequest.php
@@ -3,18 +3,24 @@
 namespace App\Http\Requests;
 
 use App\Rules\Base64;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class UserRequest extends FormRequest
 {
     public function rules(): array
     {
+        $id = request()->segment(2);
+
         return [
             'nama' => 'required',
             'telepon' => 'required|numeric',
-            'email' => 'required|email',
+            'email' => ['required', 'email', Rule::unique('user')->ignore($id)],
             'alamat' => 'required',
-            'username' => 'required',
-            'password' => 'required',
+            'lokasi' => 'nullable',
+            'role' => 'required|in:1,2,3',
+            'username' => ['required', Rule::unique('user')->ignore($id)],
+            'password' => ['required', Password::default()],
             'foto' => ['nullable', new Base64],
         ];
     }
