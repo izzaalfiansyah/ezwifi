d868732464425f902ce0f016b0b4aec1aababc2f
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-04-03 18:36:47 +0700

    make dashboard page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -18,11 +18,26 @@ export default () => {
   });
 
   return (
-    <div class="">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quas
-      accusantium explicabo illo autem provident alias impedit, officiis
-      accusamus excepturi deleniti sapiente ab voluptatem, delectus quae,
-      cupiditate beatae quam in.
-    </div>
+    <>
+      <div class="p-6 rounded bg-white shadow mb-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quas
+        accusantium explicabo illo autem provident alias impedit, officiis
+        accusamus excepturi deleniti sapiente ab voluptatem, delectus quae,
+        cupiditate beatae quam in.
+      </div>
+      <div class="p-6 rounded bg-white shadow mb-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quas
+        accusantium explicabo illo autem provident alias impedit, officiis
+        accusamus excepturi deleniti sapiente ab voluptatem, delectus quae,
+        cupiditate beatae quam in. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Sint repellat inventore, cum ad consequatur amet
+        facilis officia adipisci debitis ut esse laboriosam dolore atque qui?
+        Vel reprehenderit voluptatem praesentium deserunt. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Perferendis deleniti debitis laborum
+        vel. Incidunt asperiores assumenda dolore aut quis dolorum. Ad
+        reprehenderit placeat officiis vitae illum aliquam assumenda dolorum
+        quia.
+      </div>
+    </>
   );
 };
